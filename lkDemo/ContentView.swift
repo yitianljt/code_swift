a34262b146573c1cacfba94878284f6b78b6e6f2
Jintao <jintao081@qq.com>
2025-11-16 08:50:23 +0800

    add lengthOfLongestSubstring

--- lkDemo/ContentView.swift
+++ lkDemo/ContentView.swift
@@ -9,56 +9,70 @@ import SwiftUI
 
 
 import UIKit
-
 public class ListNode {
-     public var val: Int
-     public var next: ListNode?
-     public init() { self.val = 0; self.next = nil; }
-     public init(_ val: Int) { self.val = val; self.next = nil; }
-     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
- }
-
-class Solution {
-    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        var sumNode: ListNode? = nil
-        var current: ListNode? = nil
-        var carry = 0
-        var node1 = l1
-        var node2 = l2
-        
-        while(node1 != nil || node2 != nil){
-            let num1 = node1?.val ?? 0
-            let num2 = node2?.val ?? 0
-            var sum = num1 + num2 + carry
-            if sum >= 10 {
-                carry = 1
-                sum = sum % 10
-            } else {
-                carry = 0
-            }
-            
-            let newNode = ListNode(sum)
-            if current == nil {
-                sumNode = newNode
-                current = sumNode
-            } else {
-                current!.next = newNode
-                current = current!.next
-            }
-            
-            node1 = node1?.next
-            node2 = node2?.next
-        }
-        
-        if carry > 0 {
-            let newNode = ListNode(carry)
-            current!.next = newNode
-        }
-        
-        return sumNode
-    }
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
 }
 
+class Solution {
+   func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+       var sumNode: ListNode?
+       var currentNode: ListNode?
+       var carry = 0
+       var num1 = l1
+       var num2 = l2
+       
+
+       while num1 != nil || num1 != nil || carry != 0 {
+           var sum = carry + (num1?.val ?? 0)  + (num2?.val ?? 0)
+           if sum >= 10 {
+               carry = 1
+               sum = sum % 10
+           } else {
+               carry = 0
+           }
+
+           if sumNode == nil {
+               sumNode = ListNode(sum)
+               currentNode = sumNode
+           } else {
+               currentNode?.next = ListNode(sum)
+               currentNode = currentNode?.next
+           }
+           num1 = num1?.next
+           num2 = num2?.next
+       }
+       
+       return sumNode
+   }
+    
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        var maxLength = 0
+        for i in 0..<s.count {
+            var subChars = [Character]()
+
+            for char in s[s.index(s.startIndex, offsetBy: i)...] {
+                if subChars.contains(char) {
+                    break
+                }
+                
+                subChars.append(char)
+            }
+            
+            maxLength = max(maxLength, subChars.count)
+            
+            if s.count - i <= maxLength {
+                break
+            }
+        }
+        
+        return maxLength
+    }
+
+}
 
 struct ContentView: View {
     
@@ -75,6 +89,11 @@ struct ContentView: View {
     }
     
     func testText() -> String {
+        let solution = Solution()
+
+        let maxLength = solution.lengthOfLongestSubstring("bbbbb")
+        print(maxLength)
+        
 //        let l1 = ListNode(2, ListNode(4, ListNode(3)))
 //        let l2 = ListNode(5, ListNode(6, ListNode(4)))
 //        let solution = Solution()
@@ -88,7 +107,6 @@ struct ContentView: View {
         
         let l1 = ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9, ListNode(9)))))))
         let l2 = ListNode(9, ListNode(9, ListNode(9, ListNode(9))))
-        let solution = Solution()
         var result = solution.addTwoNumbers(l1, l2)
         print(result ?? 0)
         while result != nil {
